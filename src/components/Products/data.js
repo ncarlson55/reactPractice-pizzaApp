6da62b3ce8d006f77919366eee4717728db942a0
Nico Carlson <nicocarlson285@gmail.com>
2021-06-06 22:12:02 -0500

    Adding Product component folder and data

---
+++ src/components/Products/data.js
@@ -0,0 +1,26 @@
+export const productData = [
+  {
+    img: product1,
+    alt: 'Pizza',
+    name: 'Supreme Pizza',
+    desc: 'Marinara sauce, Basil, Italian sausage, Roma Tomotoes, Olives, and Pesto',
+    price: '$19.99',
+    button: 'Add to Cart',
+  },
+  {
+    img: product2,
+    alt: 'Pizza',
+    name: 'Hawaiin Paradise',
+    desc: 'Marinara sauce, Basil, Chunked Pinapples, Cheddar Cheese, Olives',
+    price: '$16.99',
+    button: 'Add to Cart',
+  },
+  {
+    img: product3,
+    alt: 'Pizza',
+    name: 'Veggie Overload',
+    desc: 'Marinara sauce, Basil, Red Peppers, Black Olives, Roma Tomotoes, and Pesto',
+    price: '$14.99',
+    button: 'Add to Cart',
+  }
+];
---
+++ src/components/Products/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Products
+ = () => {
+    return (
+        <ProductsContainer>
+            <ProductsHeading>heading</ProductsHeading>
+            <ProductWrapper>
+                
+            </ProductWrapper>
+        </ProductsContainer>
+    )
+}
+
+export default Products
+
